85360ea5050e8fe2284514ff795b36ee7ba538d9
Ba9-043 Nguyễn Ngọc Minh <73248681+XDLMAO@users.noreply.github.com>
2022-03-09 11:48:34 +0700

    Create lab1(notfix).py

---
+++ lab1(notfix).py
@@ -0,0 +1,9 @@
+n = int(input('Number of Student in class '))
+
+for i in range(0,n):
+	N = input("Enter the fullname of student: ")
+	I = input("Enter the Student IDs: ")
+	M = input('Enter Marks: ')
+	C = input('Enter course: ')
+	C = (N, I, M)
+print(C)
