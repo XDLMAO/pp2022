fa77694039b644a9094570e0f33a118c225db88d
Ba9-043 Nguyễn Ngọc Minh <73248681+XDLMAO@users.noreply.github.com>
2022-03-16 00:24:12 +0700

    Delete lab1(notfix).py

--- lab1(notfix).py
+++
@@ -1,9 +0,0 @@
-n = int(input('Number of Student in class '))
-
-for i in range(0,n):
-	N = input("Enter the fullname of student: ")
-	I = input("Enter the Student IDs: ")
-	M = input('Enter Marks: ')
-	C = input('Enter course: ')
-	C = (N, I, M)
-print(C)
